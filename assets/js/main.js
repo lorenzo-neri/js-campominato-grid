66cc036a8563526a2fefa245d204c5a693f89114
lorenzo-neri <lorenzo21neri@gmail.com>
2023-09-01 16:02:01 +0200

    add function to create grid

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,12 +9,34 @@ Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed 
 */
 
 //al click del button si genera una griglia di gioco
-document.querySelector('button').addEventListener('click', function(e) {
-    
-    console.log('click');
+document.querySelector('button').addEventListener('click', function (e) {
 
-  });
+    //console.log('click');
 
-//griglia formata da 10 celle x 10 celle
+    //griglia formata da 10 celle x 10 celle
+    //creo una funzione per generare la griglia
+    function generateGrid(domEl, limit) {
 
-//cliccando sulla cella, la cella si colora di azzurro ed emette un messaggio in console
+        //genero la griglia 
+        for (let i = 0; i < limit; i++) {
+
+            const cellEl = document.createElement('div');
+            cellEl.className = 'cell';
+            cellEl.append(i + 1);
+            domEl.append(cellEl);
+            //console.log(cellEl);
+
+            //cliccando sulla cella, la cella si colora di azzurro ed emette un messaggio in console
+            cellEl.addEventListener('click', function () {
+                //console.log('click on', cellEl);
+                this.classList.toggle('if_click');
+            })
+        }
+    }
+
+    const gridEl = document.querySelector('.grid');
+    const limit = 100;
+    generateGrid(gridEl, limit);
+
+
+});
