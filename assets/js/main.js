236f129d580aed542260cea8a9f20b9f2f3944c5
lorenzo-neri <lorenzo21neri@gmail.com>
2023-09-01 14:57:40 +0200

    add event listener click

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,14 @@ Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
 */
+
+//al click del button si genera una griglia di gioco
+document.querySelector('button').addEventListener('click', function(e) {
+    
+    console.log('click');
+
+  });
+
+//griglia formata da 10 celle x 10 celle
+
+//cliccando sulla cella, la cella si colora di azzurro ed emette un messaggio in console
